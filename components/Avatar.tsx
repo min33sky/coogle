84e1318e8d6ac7c4fb456591f251d9713a4aaf58
min33sky <min33sky@naver.com>
2021-08-21 01:20:40 +0900

    first commit

---
+++ components/Avatar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface IAvatar {
+  url: string;
+}
+/**
+ * 아바타 컴포넌트
+ * https://stackoverflow.com/questions/64846858/how-to-use-tailwind-css-with-next-js-image
+ * @param param0
+ * @returns
+ */
+function Avatar({ url }: IAvatar) {
+  return (
+    <div className="relative w-10 h-10 transition transform cursor-pointer hover:scale-110">
+      <Image className="rounded-full" loading="lazy" src={url} alt="profile pic" layout="fill" />
+    </div>
+  );
+}
+
+export default Avatar;
